c43ce655cc066edbe999b4d8d6d31c1e616e3575
lux1177 <ualimdjanov10@gmail.com>
2025-04-29 16:01:08 +0500

    structure change

--- data/facultiesData.ts
+++ data/facultiesData.ts
@@ -2,144 +2,410 @@ import type {Building} from "~/types/building";
 
 export const faculties: Building[] = [
 	{
-		id: 1,
-		name: "Офтальмология кафедраси",
-		location: "Vizion optima, Lens optima (темир йўл вокзали) 8-OP,5-OP, VBKTTM (furqat)",
-		image: "/Furqat.jpg",
-		manager: "Мудири Икромов А.Ф. - (90) 269-01-00",
-		head_teacher: "Завуч  Умарова Б. - (91) 477-30-65",
-		location_google: "https://maps.app.goo.gl/zDdiuVu8roHggh8y6",
-		location_yandex: "https://yandex.uz/maps/-/CHVFr6zq"
+		"id": 1,
+		"name": "Офтальмология кафедраси",
+		"images": [
+			"/Furqat.jpg",
+			"/Furqat.jpg",
+			"/Furqat.jpg"
+		],
+		"locations": [
+			{
+				"id": 1,
+				"type": "private",
+				"name": "Vizion optima",
+				"manager": "Мудири Икромов А.Ф. - (90) 269-01-00",
+				"head_teacher": "Завуч Умарова Б. - (91) 477-30-65",
+				"location_google": "https://maps.app.goo.gl/zDdiuVu8roHggh8y6",
+				"location_yandex": "https://yandex.uz/maps/-/CHVFr6zq"
+			},
+			{
+				"id": 2,
+				"type": "state",
+				"name": "Lens optima (темир йўл вокзали) 8-OP",
+				"manager": "Мудири Икромов А.Ф. - (90) 269-01-00",
+				"head_teacher": "Завуч Умарова Б. - (91) 477-30-65",
+				"location_google": "https://maps.app.goo.gl/zDdiuVu8roHggh8y6",
+				"location_yandex": "https://yandex.uz/maps/-/CHVFr6zq"
+			},
+			{
+				"id": 3,
+				"type": "private",
+				"name": "5-OP",
+				"manager": "Мудири Икромов А.Ф. - (90) 269-01-00",
+				"head_teacher": "Завуч Умарова Б. - (91) 477-30-65",
+				"location_google": "https://maps.app.goo.gl/zDdiuVu8roHggh8y6",
+				"location_yandex": "https://yandex.uz/maps/-/CHVFr6zq"
+			},
+			{
+				"id": 4,
+				"type": "state",
+				"name": "VBKTTM (furqat)",
+				"manager": "Мудири Икромов А.Ф. - (90) 269-01-00",
+				"head_teacher": "Завуч Умарова Б. - (91) 477-30-65",
+				"location_google": "https://maps.app.goo.gl/zDdiuVu8roHggh8y6",
+				"location_yandex": "https://yandex.uz/maps/-/CHVFr6zq"
+			}
+		]
 	},
 	{
-		id: 2,
-		name: "Анатомия ва клиник анатомия кафедраси",
-		location: "АДТИ 1-ўқув биноси",
-		image: "/1-bino.jpg",
-		manager: "Мудири Кахаров З. - (93) 524-96-19",
-		head_teacher: "Завуч Тулянова Д. - (94) 568-11-58",
-		location_google: "https://maps.app.goo.gl/QiJNzAi17qLrf2DX7",
-		location_yandex: "https://yandex.uz/maps/-/CHVFV45v"
+		"id": 2,
+		"name": "Анатомия ва клиник анатомия кафедраси",
+		"images": [
+			"/1-bino.jpg",
+			"/1-bino.jpg",
+			"/1-bino.jpg"
+		],
+		"locations": [
+			{
+				"id": 1,
+				"type": "state",
+				"name": "АДТИ 1-ўқув биноси",
+				"manager": "Мудири Кахаров З. - (93) 524-96-19",
+				"head_teacher": "Завуч Тулянова Д. - (94) 568-11-58",
+				"location_google": "https://maps.app.goo.gl/QiJNzAi17qLrf2DX7",
+				"location_yandex": "https://yandex.uz/maps/-/CHVFV45v"
+			}
+		]
 	},
 	{
-		id: 3,
-		name: "Юкумли касалликлар кафедраси",
-		location: "Вилоят юқумли касалликлар шифохонаси",
-		image: "/yuqumli-kasalliklar.jpg",
-		manager: "Мудири Мирзакаримова Д.Б. - (90) 526-02-32",
-		head_teacher: "Завуч Каюмов А. - (97) 473-65-52",
-		location_google: "https://maps.app.goo.gl/ts36Vn19CXen79v88",
-		location_yandex: "https://yandex.uz/maps/-/CHVFv4ib"
+		"id": 3,
+		"name": "Юкумли касалликлар кафедраси",
+		"images": [
+			"/yuqumli-kasalliklar.jpg",
+			"/yuqumli-kasalliklar.jpg",
+			"/yuqumli-kasalliklar.jpg"
+		],
+		"locations": [
+			{
+				"id": 1,
+				"type": "state",
+				"name": "Вилоят юқумли касалликлар шифохонаси",
+				"manager": "Мудири Мирзакаримова Д.Б. - (90) 526-02-32",
+				"head_teacher": "Завуч Каюмов А. - (97) 473-65-52",
+				"location_google": "https://maps.app.goo.gl/ts36Vn19CXen79v88",
+				"location_yandex": "https://yandex.uz/maps/-/CHVFv4ib"
+			}
+		]
 	},
 	{
-		id: 4,
-		name: "Жаррохлик касалликлари ва фуқаролар мухофазаси  кафедраси",
-		location: "ВКТТМ (Санчас), онкология, Мусаффо лимфа клиникаси, Агиф клиникаси,6-ОП ва АДТИ 3-ўқув биноси",
-		image: "/kafedralar/musaffo-limfa.jpg",
-		manager: "Мудири Эгамов Й.С. - (90) 253-92-29",
-		head_teacher: "Завуч Азизов Дилшодбек - (90) 166-88-44",
-		location_google: "https://maps.app.goo.gl/brxTmmDw1KVLZ5zk6",
-		location_yandex: "https://yandex.uz/maps/-/CHVFv2px"
+		"id": 4,
+		"name": "Жаррохлик касалликлари ва фуқаролар мухофазаси кафедраси",
+		"images": [
+			"/kafedralar/musaffo-limfa.jpg",
+			"/kafedralar/musaffo-limfa.jpg",
+			"/kafedralar/musaffo-limfa.jpg"
+		],
+		"locations": [
+			{
+				"id": 1,
+				"type": "state",
+				"name": "ВКТТМ (Санчас)",
+				"manager": "Мудири Эгамов Й.С. - (90) 253-92-29",
+				"head_teacher": "Завуч Азизов Дилшодбек - (90) 166-88-44",
+				"location_google": "https://maps.app.goo.gl/brxTmmDw1KVLZ5zk6",
+				"location_yandex": "https://yandex.uz/maps/-/CHVFv2px"
+			},
+			{
+				"id": 2,
+				"type": "state",
+				"name": "Oнкология",
+				"manager": "Мудири Эгамов Й.С. - (90) 253-92-29",
+				"head_teacher": "Завуч Азизов Дилшодбек - (90) 166-88-44",
+				"location_google": "https://maps.app.goo.gl/brxTmmDw1KVLZ5zk6",
+				"location_yandex": "https://yandex.uz/maps/-/CHVFv2px"
+			},
+			{
+				"id": 3,
+				"type": "private",
+				"name": "Мусаффо лимфа клиникаси",
+				"manager": "Мудири Эгамов Й.С. - (90) 253-92-29",
+				"head_teacher": "Завуч Азизов Дилшодбек - (90) 166-88-44",
+				"location_google": "https://maps.app.goo.gl/brxTmmDw1KVLZ5zk6",
+				"location_yandex": "https://yandex.uz/maps/-/CHVFv2px"
+			},
+			{
+				"id": 4,
+				"type": "private",
+				"name": "Агиф клиникаси",
+				"manager": "Мудири Эгамов Й.С. - (90) 253-92-29",
+				"head_teacher": "Завуч Азизов Дилшодбек - (90) 166-88-44",
+				"location_google": "https://maps.app.goo.gl/brxTmmDw1KVLZ5zk6",
+				"location_yandex": "https://yandex.uz/maps/-/CHVFv2px"
+			},
+			{
+				"id": 5,
+				"type": "state",
+				"name": "6-ОП",
+				"manager": "Мудири Эгамов Й.С. - (90) 253-92-29",
+				"head_teacher": "Завуч Азизов Дилшодбек - (90) 166-88-44",
+				"location_google": "https://maps.app.goo.gl/brxTmmDw1KVLZ5zk6",
+				"location_yandex": "https://yandex.uz/maps/-/CHVFv2px"
+			},
+			{
+				"id": 6,
+				"type": "state",
+				"name": "АДТИ 3-ўқув биноси",
+				"manager": "Мудири Эгамов Й.С. - (90) 253-92-29",
+				"head_teacher": "Завуч Азизов Дилшодбек - (90) 166-88-44",
+				"location_google": "https://maps.app.goo.gl/brxTmmDw1KVLZ5zk6",
+				"location_yandex": "https://yandex.uz/maps/-/CHVFv2px"
+			}
+		]
 	},
 	{
-		id: 5,
-		name: "Факультет педиатрия ва неонатология кафедраси",
-		location: "ШБКТТМ (осма кўприк проспект 24-макатаб ёнида)",
-		image: "/kafedralar/bolalar-shifoxonasi.jpg",
-		manager: "Мудири Атажанова Ш. - (90) 254-38-66",
-		head_teacher: "Завуч Арзибекова У. - (91) 608-41-19",
-		location_google: "https://maps.app.goo.gl/t7YJRsaLfvJUaaV79",
-		location_yandex: "https://yandex.uz/maps/-/CHVFzZJd"
+		"id": 5,
+		"name": "Факультет педиатрия ва неонатология кафедраси",
+		"images": [
+			"/kafedralar/bolalar-shifoxonasi.jpg",
+			"/kafedralar/bolalar-shifoxonasi.jpg",
+			"/kafedralar/bolalar-shifoxonasi.jpg"
+		],
+		"locations": [
+			{
+				"id": 1,
+				"type": "state",
+				"name": "ШБКТТМ (осма кўприк проспект 24-макатаб ёнида)",
+				"manager": "Мудири Атажанова Ш. - (90) 254-38-66",
+				"head_teacher": "Завуч Арзибекова У. - (91) 608-41-19",
+				"location_google": "https://maps.app.goo.gl/t7YJRsaLfvJUaaV79",
+				"location_yandex": "https://yandex.uz/maps/-/CHVFzZJd"
+			}
+		]
 	},
 	{
-		id: 6,
-		name: "Госпитал педиатрия кафедраси",
-		location: "Вилоят кўп тармоқли болалар шифохонаси (Фуркат)",
-		image: "/Furqat.jpg",
-		manager: "Мудири Ганиева М.Ш. - (93) 443-18-53",
-		head_teacher: "Завуч Хайдарова Л. - (93) 440-96-33",
-		location_google: "https://maps.app.goo.gl/bbNu8ARejoGR3mkC9",
-		location_yandex: "https://yandex.uz/maps/-/CHVFzTlK"
+		"id": 6,
+		"name": "Госпитал педиатрия кафедраси",
+		"images": [
+			"/Furqat.jpg",
+			"/Furqat.jpg",
+			"/Furqat.jpg"
+		],
+		"locations": [
+			{
+				"id": 1,
+				"type": "state",
+				"name": "Вилоят кўп тармоқли болалар шифохонаси (Фуркат)",
+				"manager": "Мудири Ганиева М.Ш. - (93) 443-18-53",
+				"head_teacher": "Завуч Хайдарова Л. - (93) 440-96-33",
+				"location_google": "https://maps.app.goo.gl/bbNu8ARejoGR3mkC9",
+				"location_yandex": "https://yandex.uz/maps/-/CHVFzTlK"
+			}
+		]
 	},
 	{
-		id: 7,
-		name: "Болалар касалликлари пропедевтикаси ва поликлиник педиатрия кафедраси",
-		location: "1-ОП, МКТП-5-этаж ВБКТТМ (фуркат)",
-		image: "/Furqat.jpg",
-		manager: "Мудири М.Э. Абдуллаева - (90) 258-10-23",
-		head_teacher: "Завуч Р. Абдулхакова - (97) 446-82-44",
-		location_google: "https://maps.app.goo.gl/M2ZSFc1dFpeUKMUXA",
-		location_yandex: "https://yandex.uz/maps/-/CHVFzTlK"
+		"id": 7,
+		"name": "Болалар касалликлари пропедевтикаси ва поликлиник педиатрия кафедраси",
+		"images": [
+			"/Furqat.jpg",
+			"/Furqat.jpg",
+			"/Furqat.jpg"
+		],
+		"locations": [
+			{
+				"id": 1,
+				"type": "state",
+				"name": "1-ОП",
+				"manager": "Мудири М.Э. Абдуллаева - (90) 258-10-23",
+				"head_teacher": "Завуч Р. Абдулхакова - (97) 446-82-44",
+				"location_google": "https://maps.app.goo.gl/M2ZSFc1dFpeUKMUXA",
+				"location_yandex": "https://yandex.uz/maps/-/CHVFzTlK"
+			},
+			{
+				"id": 2,
+				"type": "state",
+				"name": "МКТП-5-этаж ВБКТТМ (фуркат)",
+				"manager": "Мудири М.Э. Абдуллаева - (90) 258-10-23",
+				"head_teacher": "Завуч Р. Абдулхакова - (97) 446-82-44",
+				"location_google": "https://maps.app.goo.gl/M2ZSFc1dFpeUKMUXA",
+				"location_yandex": "https://yandex.uz/maps/-/CHVFzTlK"
+			}
+		]
 	},
 	{
-		id: 8,
-		name: "Дерматовенерология кафедраси",
-		location: "Вилоят тери-таносил касалликлар шифохонаси",
-		image: "/kafedralar/derma.jpg",
-		manager: "Мудири Пакирдинов А.Б. - (90) 571-17-75",
-		head_teacher: "Завуч Кучкаров А. - (97) 380-36-33",
-		location_google: "https://maps.app.goo.gl/BThFmZpCXSz5x6nG6",
-		location_yandex: "https://yandex.uz/maps/-/CHVF7OIy"
+		"id": 8,
+		"name": "Дерматовенерология кафедраси",
+		"images": [
+			"/kafedralar/derma.jpg",
+			"/kafedralar/derma.jpg",
+			"/kafedralar/derma.jpg"
+		],
+		"locations": [
+			{
+				"id": 1,
+				"type": "state",
+				"name": "Вилоят тери-таносил касалликлар шифохонаси",
+				"manager": "Мудири Пакирдинов А.Б. - (90) 571-17-75",
+				"head_teacher": "Завуч Кучкаров А. - (97) 380-36-33",
+				"location_google": "https://maps.app.goo.gl/BThFmZpCXSz5x6nG6",
+				"location_yandex": "https://yandex.uz/maps/-/CHVF7OIy"
+			}
+		]
 	},
 	{
-		id: 9,
-		name: "Ижтимоий гигиена ва ССБ кафедраси",
-		location: "АДТИ 2-ўқув биноси",
-		image: "/1-bino.jpg",
-		manager: "Мудири Бабич С.М. - (90) 622-39-95",
-		head_teacher: "Завуч Темирова Д. - (97) 797-00-44",
-		location_google: "https://maps.app.goo.gl/QiJNzAi17qLrf2DX7",
-		location_yandex: "https://yandex.uz/maps/-/CHVFV45v"
+		"id": 9,
+		"name": "Ижтимоий гигиена ва ССБ кафедраси",
+		"images": [
+			"/1-bino.jpg",
+			"/1-bino.jpg",
+			"/1-bino.jpg"
+		],
+		"locations": [
+			{
+				"id": 1,
+				"type": "state",
+				"name": "АДТИ 2-ўқув биноси",
+				"manager": "Мудири Бабич С.М. - (90) 622-39-95",
+				"head_teacher": "Завуч Темирова Д. - (97) 797-00-44",
+				"location_google": "https://maps.app.goo.gl/QiJNzAi17qLrf2DX7",
+				"location_yandex": "https://yandex.uz/maps/-/CHVFV45v"
+			}
+		]
 	},
 	{
-		id: 10,
-		name: "Превентив тиббиёт асослари кафедраси",
-		location: "АДТИ 2-ўқув биноси",
-		image: "/1-bino.jpg",
-		manager: "Мудири Салиева М.Х. - (91) 612-65-02",
-		head_teacher: "Завуч Хожиахматова Р. - (93) 215-91-61",
-		location_google: "https://maps.app.goo.gl/QiJNzAi17qLrf2DX7",
-		location_yandex: "https://yandex.uz/maps/-/CHVFV45v"
+		"id": 10,
+		"name": "Превентив тиббиёт асослари кафедраси",
+		"images": [
+			"/1-bino.jpg",
+			"/1-bino.jpg",
+			"/1-bino.jpg"
+		],
+		"locations": [
+			{
+				"id": 1,
+				"type": "state",
+				"name": "АДТИ 2-ўқув биноси",
+				"manager": "Мудири Салиева М.Х. - (91) 612-65-02",
+				"head_teacher": "Завуч Хожиахматова Р. - (93) 215-91-61",
+				"location_google": "https://maps.app.goo.gl/QiJNzAi17qLrf2DX7",
+				"location_yandex": "https://yandex.uz/maps/-/CHVFV45v"
+			}
+		]
 	},
 	{
-		id: 11,
-		name: "Болалар жаррохлиги кафедраси",
-		location: "Вилоят кўп тармоқли болалар шифохонаси (фуркат) РШТТЁАФ (облосной)",
-		image: "/Furqat.jpg",
-		manager: "Мудири Мирзакаримов Б.Х. - (91) 606-12-73",
-		head_teacher: "Завуч Юлдашев М. - (91) 499-46-56",
-		location_google: "https://maps.app.goo.gl/bbNu8ARejoGR3mkC9",
-		location_yandex: "https://yandex.uz/maps/-/CHVFzTlK"
+		"id": 11,
+		"name": "Болалар жаррохлиги кафедраси",
+		"images": [
+			"/Furqat.jpg",
+			"/Furqat.jpg",
+			"/Furqat.jpg"
+		],
+		"locations": [
+			{
+				"id": 1,
+				"type": "state",
+				"name": "Вилоят кўп тармоқли болалар шифохонаси (фуркат) РШТТЁАФ (облосной)",
+				"manager": "Мудири Мирзакаримов Б.Х. - (91) 606-12-73",
+				"head_teacher": "Завуч Юлдашев М. - (91) 499-46-56",
+				"location_google": "https://maps.app.goo.gl/bbNu8ARejoGR3mkC9",
+				"location_yandex": "https://yandex.uz/maps/-/CHVFzTlK"
+			}
+		]
 	},
 	{
-		id: 12,
-		name: "1-Акушерлик ва гинекология кафедраси",
-		location: "Вилоят перенатал маркази (семашко) ва Family med klinikasi",
-		image: "/Perenatal_markaz.jpg",
-		manager: "Мудири Асранкулова Д.Б. - (91) 167-23-30",
-		head_teacher: "Завуч Абдуллаева М. - (91) 493-25-88",
-		location_google: "https://maps.app.goo.gl/1DgTUe2Bv9JRotiP6",
-		location_yandex: "https://yandex.uz/maps/-/CHVJAC9C"
+		"id": 12,
+		"name": "1-Акушерлик ва гинекология кафедраси",
+		"images": [
+			"/Perenatal_markaz.jpg",
+			"/Perenatal_markaz.jpg",
+			"/Perenatal_markaz.jpg"
+		],
+		"locations": [
+			{
+				"id": 1,
+				"type": "state",
+				"name": "Вилоят перенатал маркази (семашко)",
+				"manager": "Мудири Асранкулова Д.Б. - (91) 167-23-30",
+				"head_teacher": "Завуч Абдуллаева М. - (91) 493-25-88",
+				"location_google": "https://maps.app.goo.gl/1DgTUe2Bv9JRotiP6",
+				"location_yandex": "https://yandex.uz/maps/-/CHVJAC9C"
+			},
+			{
+				"id": 2,
+				"type": "private",
+				"name": "Family med клиникаси",
+				"manager": "Мудири Асранкулова Д.Б. - (91) 167-23-30",
+				"head_teacher": "Завуч Абдуллаева М. - (91) 493-25-88",
+				"location_google": "https://maps.app.goo.gl/1DgTUe2Bv9JRotiP6",
+				"location_yandex": "https://yandex.uz/maps/-/CHVJAC9C"
+			}
+		]
 	},
 	{
-		id: 13,
-		name: "Болалар травматологияси, ортопедияси ва нейрожаррохлик кафедраси",
-		location: "Телмон вилоят травматалогия шифохонаси, РШТТЁАФ (облосной), Сехат клиникаси ва Ибн сино клиникаси",
-		image: "/kafedralar/sehat.jpg",
-		manager: "Мудири Кулдашев К.А. - (97) 995-00-01",
-		head_teacher: "Завуч Расулов Жахонгир - (93) 855-77-88",
-		location_google: "https://maps.app.goo.gl/QDMaJVx8Nk5tGKMcA",
-		location_yandex: "https://yandex.uz/maps/-/CHVJA28b"
+		"id": 13,
+		"name": "Болалар травматологияси, ортопедияси ва нейрожаррохлик кафедраси",
+		"images": [
+			"/kafedralar/sehat.jpg",
+			"/kafedralar/sehat.jpg",
+			"/kafedralar/sehat.jpg"
+		],
+		"locations": [
+			{
+				"id": 1,
+				"type": "state",
+				"name": "Телмон вилоят травматалогия шифохонаси",
+				"manager": "Мудири Кулдашев К.А. - (97) 995-00-01",
+				"head_teacher": "Завуч Расулов Жахонгир - (93) 855-77-88",
+				"location_google": "https://maps.app.goo.gl/QDMaJVx8Nk5tGKMcA",
+				"location_yandex": "https://yandex.uz/maps/-/CHVJA28b"
+			},
+			{
+				"id": 2,
+				"type": "state",
+				"name": "РШТТЁАФ (облосной)",
+				"manager": "Мудири Кулдашев К.А. - (97) 995-00-01",
+				"head_teacher": "Завуч Расулов Жахонгир - (93) 855-77-88",
+				"location_google": "https://maps.app.goo.gl/QDMaJVx8Nk5tGKMcA",
+				"location_yandex": "https://yandex.uz/maps/-/CHVJA28b"
+			},
+			{
+				"id": 3,
+				"type": "private",
+				"name": "Сехат клиникаси",
+				"manager": "Мудири Кулдашев К.А. - (97) 995-00-01",
+				"head_teacher": "Завуч Расулов Жахонгир - (93) 855-77-88",
+				"location_google": "https://maps.app.goo.gl/QDMaJVx8Nk5tGKMcA",
+				"location_yandex": "https://yandex.uz/maps/-/CHVJA28b"
+			},
+			{
+				"id": 4,
+				"type": "private",
+				"name": "Ибн сино клиникаси",
+				"manager": "Мудири Кулдашев К.А. - (97) 995-00-01",
+				"head_teacher": "Завуч Расулов Жахонгир - (93) 855-77-88",
+				"location_google": "https://maps.app.goo.gl/QDMaJVx8Nk5tGKMcA",
+				"location_yandex": "https://yandex.uz/maps/-/CHVJA28b"
+			}
+		]
 	},
 	{
-		id: 14,
-		name: "Педиатрия факультети учун ички касалликлари пропедевтикаси",
-		location: "ШКТТМ (санчас) ва Вилоят кардиология маркази",
-		image: "/kafedralar/sanchas.jpg",
-		manager: "Мудири Артикова С.Г.  - (99) 316-71-00",
-		head_teacher: "Завуч Юнусова Зебо - (93) 427-42-22",
-		location_google: "https://maps.app.goo.gl/VmUYPURQQuq6rYzS9",
-		location_yandex: "https://yandex.uz/maps/-/CHVJE4N0"
+		"id": 14,
+		"name": "Педиатрия факультети учун ички касалликлари пропедевтикаси",
+		"images": [
+			"/kafedralar/sanchas.jpg",
+			"/kafedralar/sanchas.jpg",
+			"/kafedralar/sanchas.jpg"
+		],
+		"locations": [
+			{
+				"id": 1,
+				"type": "state",
+				"name": "ШКТТМ (санчас)",
+				"manager": "Мудири Артикова С.Г. - (99) 316-71-00",
+				"head_teacher": "Завуч Юнусова Зебо - (93) 427-42-22",
+				"location_google": "https://maps.app.goo.gl/VmUYPURQQuq6rYzS9",
+				"location_yandex": "https://yandex.uz/maps/-/CHVJE4N0"
+			},
+			{
+				"id": 2,
+				"type": "state",
+				"name": "Вилоят кардиология маркази",
+				"manager": "Мудири Артикова С.Г. - (99) 316-71-00",
+				"head_teacher": "Завуч Юнусова Зебо - (93) 427-42-22",
+				"location_google": "https://maps.app.goo.gl/VmUYPURQQuq6rYzS9",
+				"location_yandex": "https://yandex.uz/maps/-/CHVJE4N0"
+			}
+		]
 	},
 	{
 		id: 15,
