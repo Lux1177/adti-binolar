f10c9ff1092eb4e92ed1b5683a0b7bff2a38e091
lux1177 <ualimdjanov10@gmail.com>
2025-08-22 09:32:26 +0500

    added website icon

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,7 +3,30 @@ export default defineNuxtConfig({
   modules: ['@nuxt/ui', '@nuxt/image'],
   compatibilityDate: '2024-11-01',
   app: {
-    pageTransition: { name: 'page', mode: 'out-in' }
+    pageTransition: { name: 'page', mode: 'out-in' },
+	  head: {
+		  htmlAttrs: {
+			  lang: "uz",
+		  },
+		  title: 'АДТИ - Бинолар',
+		  meta: [
+			  { charset: 'utf-8' },
+			  { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+			  { name: 'description', content: 'АДТИ - Бинолар жойлашуви' },
+			  { name: 'author', content: 'Umarjon Alimdjanov' },
+
+			  // Open Graph (для соцсетей)
+			  { property: 'og:title', content: 'АДТИ - Бинолар' },
+			  { property: 'og:description', content: 'АДТИ - Бинолар жойлашуви' },
+			  { property: 'og:type', content: 'website' },
+			  { property: 'og:url', content: 'https://adti-binolar.vercel.app' },
+			  {property: 'og:image', content: '/icon.png'},
+
+		  ],
+		  link: [
+			  {rel: 'icon', type: 'image/x-icon', href: '/icon.png'}
+		  ]
+	  },
   },
   experimental: {
     viewTransition: true
